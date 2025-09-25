0436691a6362196e0c110846608efdcba663bfa4
xing <xingxerx@gmail.com>
2025-09-25 12:23:58 -0400

    Add new libraries and update incremental build files
    - Added new rmeta and rlib files for `coreria`, `gl`, `gl_generator`, `khronos_api`, and `xml` libraries.
    - Created new dependency graph, query cache, and work products binary files for incremental builds.
    - Updated log library with new rmeta and rlib files.
    - Generated new dependency files for log and xml libraries.

---
+++ target/debug/build/khronos_api-b4ed1ce501f3c534/build_script_build-b4ed1ce501f3c534.d
@@ -0,0 +1,5 @@
+D:\Coreria\target\debug\build\khronos_api-b4ed1ce501f3c534\build_script_build-b4ed1ce501f3c534.d: C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\khronos_api-3.1.0\build.rs
+
+D:\Coreria\target\debug\build\khronos_api-b4ed1ce501f3c534\build_script_build-b4ed1ce501f3c534.exe: C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\khronos_api-3.1.0\build.rs
+
+C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\khronos_api-3.1.0\build.rs:
---
+++ target/debug/deps/gl-02b5fa3b8d1deadc.d
@@ -0,0 +1,8 @@
+D:\Coreria\target\debug\deps\gl-02b5fa3b8d1deadc.d: C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\gl-0.14.0\src\lib.rs D:\Coreria\target\debug\build\gl-d9bc90c879c83df0\out/bindings.rs
+
+D:\Coreria\target\debug\deps\libgl-02b5fa3b8d1deadc.rmeta: C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\gl-0.14.0\src\lib.rs D:\Coreria\target\debug\build\gl-d9bc90c879c83df0\out/bindings.rs
+
+C:\Users\there\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\gl-0.14.0\src\lib.rs:
+D:\Coreria\target\debug\build\gl-d9bc90c879c83df0\out/bindings.rs:
+
+# env-dep:OUT_DIR=D:\\Coreria\\target\\debug\\build\\gl-d9bc90c879c83df0\\out
